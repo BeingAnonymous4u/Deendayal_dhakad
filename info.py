4477bf087f3a0671ebf0fea17ce55757ebb20ff6
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-02-26 01:44:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,9 +62,9 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://herukoid1:GNli5O0mVUA4DPVjcluster0.rqded.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # ============================
 # Movie Notification & Update Settings
